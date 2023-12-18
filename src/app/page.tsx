fb4694c6ea63e170808cb94879fc9e35e6304ca3
Anatoly Boronilov <aboronilov@express42.com>
2023-12-18 17:24:27 +0300

    card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,51 @@
-export default function Home() {
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+interface Recipe {
+  title: string;
+  image: string;
+  time: number;
+  description: string;
+  vegan: boolean;
+  id: string;
+}
+
+const getRecipes = async (): Promise<Recipe[]> => {
+  const result = await fetch("http://localhost:4000/recipes")
+
+  return result.json()
+}
+
+export default async function Home() {
+  const recipes = await getRecipes()
   return (
-    <main>Home</main>
+    <main>
+      <div className="grid grid-cols-3 gap-8">
+        {recipes.map((item) => (
+          <Card key={item.id} className="flex flex-col justify-between">
+            <CardHeader className="flex gap-4 items-center">
+              {/* Avatar */}
+              <div>
+                <CardTitle>{item.title}</CardTitle>
+                <CardDescription>{item.time} mins to cook</CardDescription>
+              </div>
+            </CardHeader>
+            <CardContent>
+              <p>{item.description}</p>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <button>View details</button>
+              {item.vegan && <p>Vegan</p>}
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </main>
   )
 }
