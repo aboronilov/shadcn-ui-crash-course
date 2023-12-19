c6ef4488dfed235a39091985eb619459afe97932
Anatoly Boronilov <aboronilov@express42.com>
2023-12-19 15:51:29 +0300

    badge button and avatar UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,10 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+
 
 interface Recipe {
   title: string;
@@ -29,8 +33,13 @@ export default async function Home() {
       <div className="grid grid-cols-3 gap-8">
         {recipes.map((item) => (
           <Card key={item.id} className="flex flex-col justify-between">
-            <CardHeader className="flex gap-4 items-center">
-              {/* Avatar */}
+            <CardHeader className="flex-row gap-4 items-center">
+              <Avatar>
+                <AvatarImage src={`/img/${item.image}`} alt="recipe img"/>
+                <AvatarFallback>
+                  {item.title.slice(0, 2)}
+                </AvatarFallback>
+              </Avatar>
               <div>
                 <CardTitle>{item.title}</CardTitle>
                 <CardDescription>{item.time} mins to cook</CardDescription>
@@ -40,8 +49,8 @@ export default async function Home() {
               <p>{item.description}</p>
             </CardContent>
             <CardFooter className="flex justify-between">
-              <button>View details</button>
-              {item.vegan && <p>Vegan</p>}
+              <Button>View details</Button>
+              {item.vegan && <Badge variant="secondary">Vegan</Badge>}
             </CardFooter>
           </Card>
         ))}
